f6b8bf811b517df23517a6620188190cef1ace43
Allyson Gerald <allyson.gerald@gmail.com>
2024-06-09 23:59:25 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
 import "./styles/App.css";
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
 
 function App() {
+
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    // Este efeito salva as tarefas no localStorage sempre que o estado 'tasks' muda
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = (task) => {
+    setTasks([...tasks, { id: Date.now(), text: task, done: false }]);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  const toggleTaskDone = (taskId) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, done: !task.done } : task
+      )
+    );
+  };
+
   return (
     <div>
     </div>
